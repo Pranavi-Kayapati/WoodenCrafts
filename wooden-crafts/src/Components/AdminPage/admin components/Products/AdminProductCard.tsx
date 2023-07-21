3b79306d87e627f564d291280f53a35328fe2838
Pranavi-Kayapati <pranavikayapati01@gmail.com>
2023-07-21 19:25:34 +0530

    action

--- wooden-crafts/src/Components/AdminPage/admin components/Products/AdminProductCard.tsx
+++ wooden-crafts/src/Components/AdminPage/admin components/Products/AdminProductCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Product } from "../../../constrainsts/Type";
-
+import { MdEditNote } from "react-icons/md";
+import { RiDeleteBinLine } from "react-icons/ri";
 const AdminProductCard = ({ item }: any) => {
   const {
     id,
@@ -16,11 +17,60 @@ const AdminProductCard = ({ item }: any) => {
 
   return (
     <div className="product-card">
-      <div>
+      <div className="product-card-img-div">
         <img src={image} alt={title} />
+      </div>
+      <div className="product-card-details">
+        <h3 className="product-title">{title}</h3>
+        <p>
+          <span>Brand :</span>
+          {brand}
+        </p>
+        <div className="product-card-category">
+          <p>
+            <span>Category :</span>
+            {category}
+          </p>
+          <span>{`(${material})`}</span>
+        </div>
+
+        <p>
+          <span>Color :</span>
+          {color}
+        </p>
+        <p>
+          <span>Price :</span>
+          <span>Rs.</span>
+          {price}
+        </p>
+      </div>
+      <div>
+        <button className="sub-head">
+          <MdEditNote />
+          <span className="padd-15">Edit</span>
+        </button>
+        <button className="sub-head">
+          <RiDeleteBinLine />
+          <span className="padd-15">Delete</span>
+        </button>
       </div>
     </div>
   );
 };
 
 export default AdminProductCard;
+
+// {
+//   "id": 5,
+//   "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwBA_U1y51VPFIBePwgPgoSQWmhtGlRZSERA&usqp=CAU",
+//   "category": "Fabric Sofas",
+//   "material": "Cotton",
+//   "color": "Grey",
+//   "brand": "Urban Living ",
+//   "title": "Swanson Chesterfield Sofa",
+//   "price": "25,000",
+//   "description": {
+//     "Monsoon Sale": " Apply Coupon MONSOON & Get Upto 20% Off (price inclusive of discount) T&C",
+//     "Store Discount": "Get upto 10% off on orders placed at your nearest experience stores T&C",
+//     "No Cost EMI": "Starting from Rs 5,269 on ICICI, Axis, Kotak, HDFC, SBI & More"
+//   }
