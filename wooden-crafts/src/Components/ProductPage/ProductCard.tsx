21b291aa6afbcd93b918b79a8c9aad8a954b767b
Aditya tiwari <at257645@gmail.com>
2023-07-22 01:07:21 +0530

    day-3

--- wooden-crafts/src/Components/ProductPage/ProductCard.tsx
+++ wooden-crafts/src/Components/ProductPage/ProductCard.tsx
@@ -1,8 +1,56 @@
-export let ProductCard=()=>{
+import "./ProductCard.css"
+import { Box,Badge,Image, Button} from '@chakra-ui/react'
+import {StarIcon} from "@chakra-ui/icons"
+import { Link } from '@chakra-ui/react'
+
+
+export let ProductCard=({image,title,price,id}:any)=>{
 
     return (
         <div>
+        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'  w="95%">
+         
+        
+          <Box p='6' >
+          <Link href={`/product/${id}`}><Image src={image}  w="300px" h="200px"/></Link>
+    
+            <Box
+              mt='1'
+              fontWeight='semibold'
+              as='h4'
+              lineHeight='tight'
+              noOfLines={1}
+            >
+              {title}
+            </Box>
+                <p style={{color:"gray"}}>Wooden Street</p>
             
+            <div style={{position:"relative",top:"10px"}}>
+            <Box display='flex' mt='2' alignItems='center' >
+              {Array(5)
+                .fill('')
+                .map((_, i) => (
+                  <StarIcon
+                    key={i}
+                    color={ '#F3601E'}
+                  />
+                ))}
+              
+            </Box>
+            
+            <Box mr="200px" mt="3">
+              Rs {price}
+              
+            </Box>
+            </div>
+            <div style={{position:"relative",bottom:"30px"}}>
+            <Box ml="150px" >
+                <Button background="#F3601E" color="white" w="18vh" h="6vh">Add To Cart</Button>
+            </Box>
+            </div>
+          </Box>
+         
+        </Box>
         </div>
-    )
+      )
 }
--- wooden-crafts/src/index.tsx
+++ wooden-crafts/src/index.tsx
@@ -5,16 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./Components/redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
   </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
