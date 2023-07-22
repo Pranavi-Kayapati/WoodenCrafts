37bfdbbab3beb66055893aa23efc6246e123ebe4
Aditya tiwari <at257645@gmail.com>
2023-07-22 15:59:42 +0530

    day-4

--- wooden-crafts/src/Components/ProductPage/Sidebar.tsx
+++ wooden-crafts/src/Components/ProductPage/Sidebar.tsx
@@ -9,35 +9,76 @@ const Sidebar=()=>{
     let initOrder=search.get("order")
     let [order,setOrder]=useState(""||initOrder)
     let initPrice=search.getAll("price")
-    let [price,setPrice]=useState<string[]>([]||initPrice)
-
+    const [priceRange, setPriceRange] = useState<string[]>([]);
+    let initMa=search.getAll("material")
+    let [material,setMaterial]=useState<string[]>(initMa||[])
+    let initColor=search.getAll("color")
+    let [color,setColor]=useState<string[]>(initColor||[])
+    let initBrand=search.getAll("brand")
+    let [brand,setBrand]=useState<string[]>(initBrand||[])
 
     const HandleChange=(e:any)=>{
         setOrder(e.target.value)
     }
 
-    // const HandlePrice=(e:React.ChangeEvent<HTMLInputElement>)=>{
-    //     let {value}=e.target
-    //     let newPrice=[...price];
-    //     if(newPrice.includes(value)){
-    //             newPrice=newPrice.filter((ele)=>ele!==value)
-    //     }
-    //     else {
-    //         newPrice.push(value)
-    //     }
-    //     setPrice(newPrice)
-    // }
+    const handlePriceRange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        let newPriceRange = [...priceRange];
+        if (newPriceRange.includes(value)) {
+          newPriceRange = newPriceRange.filter((ele) => ele !== value);
+        } else {
+          newPriceRange.push(value);
+        }
+        setPriceRange(newPriceRange);
+      };
     
+    let HandelMa=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        let {value}=e.target
+        let newMaterial=[...material]
+        if(newMaterial.includes(value)){
+                newMaterial=newMaterial.filter((ele)=>ele!==value)
+        }
+        else {
+            newMaterial.push(value)
+        }
+        setMaterial(newMaterial)
+    }
   
+    let Handelcolor=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        let {value}=e.target
+        let newColor=[...color]
+        if(newColor.includes(value)){
+            newColor=newColor.filter((ele)=>ele!==value)
+        }
+        else {
+            newColor.push(value)
+        }
+        setColor(newColor)
+    }
+
+    const HandleBrand=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        let {value}=e.target;
+        let newBrand=[...brand];
+        if(newBrand.includes(value)){
+            newBrand=newBrand.filter((ele)=>ele!==value)
+        }
+        else{
+            newBrand.push(value)
+        }
+        setBrand(newBrand)
+    }
 
     useEffect(()=>{
         let params:any={
-            price: price.map((p) => p.replace(",", "")),
+            material,
+            color,
+            brand,
+            price: priceRange,
         }
 
         order && (params.order=order)
         setSearch(params)
-    },[order,price])
+    },[order,material,color,brand,priceRange])
 
    
       
@@ -64,23 +105,23 @@ const Sidebar=()=>{
             <h2>PRICE RANGE</h2>
             <br />
             <div>
-                <input type="checkbox" /> 
+                <input type="checkbox" value="19,999"/> 
                 <label >Under ₹ 19,999</label>
             </div>
             <div>
-                <input type="checkbox" /> 
+                <input type="checkbox" value="20,000 - 29,999" /> 
                 <label >₹ 20,000 - ₹ 29,999</label>
             </div>
             <div>
-                <input type="checkbox" /> 
+                <input type="checkbox" value="30,000 - 39,999"/> 
                 <label >₹ 30,000 - ₹ 39,999</label>
             </div>
             <div>
-                <input type="checkbox" /> 
+                <input type="checkbox" value="40,000 - 49,999"/> 
                 <label >₹ 40,000 - ₹ 49,999</label>
             </div>
             <div>
-                <input type="checkbox" /> 
+                <input type="checkbox" value="50,000+"/> 
                 <label >Over ₹ 50,000</label>
             </div>
             <br />
@@ -89,15 +130,15 @@ const Sidebar=()=>{
             <h3>MATERIAL</h3>
             <br />
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Velvet"} checked={material.includes("Velvet")} onChange={HandelMa}/> 
                 <label >Velvet</label>
             </div>
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Cotton"} checked={material.includes("Cotton")} onChange={HandelMa}/> 
                 <label >Cotton</label>
             </div>
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Leather"} checked={material.includes("Leather")} onChange={HandelMa}/> 
                 <label >Leather</label>
             </div>
             <br />
@@ -105,17 +146,20 @@ const Sidebar=()=>{
             <br />
             <h3>COLOR</h3>
             <br />
-            <div>
-                <input type="checkbox"/> 
-                <label >Cream</label>
+            <div style={{position:"relative",top:"33px"}}>
+                <input style={{marginRight:"11px"}} type="checkbox" value={"Cream"} onChange={Handelcolor} checked={color.includes("Cream")}/> 
+                <span style={{position:"relative",left:"31px",bottom:"20px"}}><img src="https://images.woodenstreet.de/image/data/color_group_name/CREAM.jpg" style={{width:"15px",borderRadius:"50%"}}/></span>
+                <label style={{position:"relative",left:"50px",bottom:"40px"}}>Cream</label>
             </div>
             <div>
-                <input type="checkbox"/> 
-                <label >Grey</label>
+                <input type="checkbox" value={"Grey"} onChange={Handelcolor} checked={color.includes("Grey")}/> 
+                <span style={{position:"relative",left:"30px",bottom:"20px"}}><img src="https://images.woodenstreet.de/image/data/color_group_name/grey.jpg" style={{width:"15px",borderRadius:"50%"}}/></span>
+                <label style={{position:"relative",left:"50px",bottom:"40px"}}>Grey</label>
             </div>
-            <div>
-                <input type="checkbox"/> 
-                <label >Blue</label>
+            <div style={{position:"relative",bottom:"35px",left:"1px"}}>
+                <input type="checkbox" value={"Blue"} onChange={Handelcolor} checked={color.includes("Blue")}/> 
+                <span style={{position:"relative",left:"30px",bottom:"20px"}}><img src="https://images.woodenstreet.de/image/data/color_group_name/BLUE.jpg" style={{width:"15px",borderRadius:"50%"}}/></span>
+                <label style={{position:"relative",left:"50px",bottom:"40px"}}>Blue</label>
             </div>
             <br />
             <hr />
@@ -123,19 +167,19 @@ const Sidebar=()=>{
             <h3>BRAND</h3>
             <br />
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Wooden Street"} onChange={HandleBrand} checked={brand.includes("Wooden Street")}/> 
                 <label >Wooden Street</label>
             </div>
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Urban Living"} onChange={HandleBrand} checked={brand.includes("Urban Living")}/> 
                 <label >Urban Living</label>
             </div>
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Arra"} onChange={HandleBrand} checked={brand.includes("Arra")}/> 
                 <label >Arra</label>
             </div>
             <div>
-                <input type="checkbox"/> 
+                <input type="checkbox" value={"Spacewood"} onChange={HandleBrand} checked={brand.includes("Spacewood")}/> 
                 <label >Spacewood</label>
             </div>
         </div>
