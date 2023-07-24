467d5e14953bf3a68d6195f9386dd851393d131f
Pranavi-Kayapati <pranavikayapati01@gmail.com>
2023-07-24 11:36:34 +0530

    add

--- wooden-crafts/src/Components/CartPage/CartItem.tsx
+++ wooden-crafts/src/Components/CartPage/CartItem.tsx
@@ -27,53 +27,53 @@ const del = (id: number) => {
     })
     .catch((err) => {
       console.log(err);
+    });
+};
 
-    })
-    
-}
+const CartItem: React.FC<CartItemProps> = ({
+  id,
+  product,
+  quantity = 1,
+  setTotal,
+  setData,
+}) => {
+  const dispatch = useDispatch();
 
-const CartItem:React.FC<CartItemProps> = ({id,product,quantity=1,setTotal,setData}) => {
-  const dispatch=useDispatch()
- 
-  const [count,setCount]=useState<number>(1)
-
+  const [count, setCount] = useState<number>(1);
 
   console.log(product.price);
   let price: any = product.price.split(",").join("");
-  price=Number(price)
-  console.log(price)
+  price = Number(price);
+  console.log(price);
 
-  useEffect(()=>{
+  useEffect(() => {
     //setTotal((prev:any)=>prev+price)
-  },[count])
+  }, [count]);
 
- 
-  const FetchData=()=>{
- 
-    axios.get("https://all-products-wjqd.onrender.com/cart")
-    .then((res)=>{
-      console.log("product is Removed")
-      console.log(res.data)
-      setData(res.data)
-     
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  
-   }
+  const FetchData = () => {
+    axios
+      .get("https://all-products-wjqd.onrender.com/cart")
+      .then((res) => {
+        console.log("product is Removed");
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const handleRemoveFromCart = async () => {
     //del(id)
-    axios.delete(`https://all-products-wjqd.onrender.com/cart/${id}`)
-    .then((res)=>{
-      FetchData()
-      return res
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-   
+    axios
+      .delete(`https://all-products-wjqd.onrender.com/cart/${id}`)
+      .then((res) => {
+        FetchData();
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const handleIncreaseQuantity = () => {
@@ -84,156 +84,166 @@ const CartItem:React.FC<CartItemProps> = ({id,product,quantity=1,setTotal,setDat
     //   dispatch(increaseQuantity(res.data))
 
     let newprice: any = product.price.split(",").join("");
-        newprice=Number(newprice)
+    newprice = Number(newprice);
 
-    setCount(prev=>prev+1)
-   
-    setTotal((prev:any)=>prev+newprice)
+    setCount((prev) => prev + 1);
+
+    setTotal((prev: any) => prev + newprice);
     //dispatch(increaseQuantity(product.id))
   };
 
   const handleDecreaseQuantity = () => {
-    console.log("work")
-    console.log(price)
+    console.log("work");
+    console.log(price);
 
-    setCount(prev=>prev-1)
-    setTotal((prev:any)=>prev-price)
-   // dispatch(decreaseQuantity(product.id));
+    setCount((prev) => prev - 1);
+    setTotal((prev: any) => prev - price);
+    // dispatch(decreaseQuantity(product.id));
   };
   return (
     <DIV>
-         <hr/>
+      <hr />
 
-<div className="cartItem">
-            <div className="cartItemimage">
-              <img src={product.image} alt="ErrorImage"/>
-            </div>
-            <div className="cartItemdetail">
-              <div className="cartTitle"><h1><b>{product.title}</b></h1></div>
-              <div className="cartQantity"><span>Quantity
-               <span className="incDec">
-                <button disabled={count===1} onClick={handleDecreaseQuantity}>-</button><span className="qauntity">{count}</span>
-                <button disabled={count===5} onClick={handleIncreaseQuantity}>+</button>
-               </span>
-               <span className="price"><b>{`Rs ${price}`}</b> <span className="discount">Rs 100</span></span>
-               <span className="cuponCode"><span className="saving">Save Rs 119 </span><h1>After applying coupon "MONSOON"</h1></span></span>
-               </div>
-              <div className="cartButton"><span className="saveforlater">Save for later</span> <span className="remove" onClick={handleRemoveFromCart}>Remove</span></div>
-              
-            </div>
+      <div className="cartItem">
+        <div className="cartItemimage">
+          <img src={product.image} alt="ErrorImage" />
+        </div>
+        <div className="cartItemdetail">
+          <div className="cartTitle">
+            <h1>
+              <b>{product.title}</b>
+            </h1>
           </div>
-
+          <div className="cartQantity">
+            <span>
+              Quantity
+              <span className="incDec">
+                <button disabled={count === 1} onClick={handleDecreaseQuantity}>
+                  -
+                </button>
+                <span className="qauntity">{count}</span>
+                <button disabled={count === 5} onClick={handleIncreaseQuantity}>
+                  +
+                </button>
+              </span>
+              <span className="price">
+                <b>{`Rs ${price}`}</b> <span className="discount">Rs 100</span>
+              </span>
+              <span className="cuponCode">
+                <span className="saving">Save Rs 119 </span>
+                <h1>After applying coupon "MONSOON"</h1>
+              </span>
+            </span>
+          </div>
+          <div className="cartButton">
+            <span className="saveforlater">Save for later</span>{" "}
+            <span className="remove" onClick={handleRemoveFromCart}>
+              Remove
+            </span>
+          </div>
+        </div>
+      </div>
     </DIV>
-    
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
 
-const DIV=styled.div`
-    
-    
-  
-.cartItem{
-  margin-top:15px;
-  width: 100%;
-  height: 200px;
-  display: flex;
-  gap:10px;
- 
-}
+const DIV = styled.div`
+  .cartItem {
+    margin-top: 15px;
+    width: 100%;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
 
-.cartItemimage{
-  width: 30%;
-  justify-content:center;
-  align-items:center;
-  position: relative;
- 
-}
-.cartItemimage img{
-  position: absolute;
-  left: 0px;
-  top:20px;
-}
-.cartItemdetail{
-  width:70%;
-  position: relative;
-}
- 
-.cartItemdetail div{
-  position: absolute;
-  left: 0px;
-  
-}
+  .cartItemimage {
+    width: 30%;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  .cartItemimage img {
+    position: absolute;
+    left: 0px;
+    top: 20px;
+  }
+  .cartItemdetail {
+    width: 70%;
+    position: relative;
+  }
 
-.cartTitle{
-  position:absolute;
-  top:20px;
-}
+  .cartItemdetail div {
+    position: absolute;
+    left: 0px;
+  }
 
-.cartQantity{
-  position:absolute;
-  top:80px;
-  
-}
-.cartButton{
-  position:absolute;
-  bottom:20px;
-}
+  .cartTitle {
+    position: absolute;
+    top: 20px;
+  }
 
-.qant{
-  padding: 0px;
-  margin-left:15px;
-  margin-right:10px;
-  border:1px solid grey;
-  padding:4px;
-}
+  .cartQantity {
+    position: absolute;
+    top: 80px;
+  }
+  .cartButton {
+    position: absolute;
+    bottom: 20px;
+  }
 
-.incDec{
-  margin: 10px;
-  border: 1px solid grey;
-  width: 30px;
-  padding: 5px;
-}
-.incDec button{
-  margin-left:10px;
-  margin-right:10px;
-  padding-left:2px;
-  padding-right:2px;
+  .qant {
+    padding: 0px;
+    margin-left: 15px;
+    margin-right: 10px;
+    border: 1px solid grey;
+    padding: 4px;
+  }
 
-}
+  .incDec {
+    margin: 10px;
+    border: 1px solid grey;
+    width: 30px;
+    padding: 5px;
+  }
+  .incDec button {
+    margin-left: 10px;
+    margin-right: 10px;
+    padding-left: 2px;
+    padding-right: 2px;
+  }
 
-.incDec button:hover{
-  color: #FF9800;
-}
-.qauntity{
-  border:1px solid gray;
-  padding:5px 7px;
+  .incDec button:hover {
+    color: #ff9800;
+  }
+  .qauntity {
+    border: 1px solid gray;
+    padding: 5px 7px;
+  }
+  .price {
+    margin-left: 5px;
+  }
+  .discount {
+    margin-left: 5px;
+  }
+  .cuponCode {
+    margin-left: 15px;
+  }
+  .saving {
+    color: orange;
+  }
+  .cuponCode h1 {
+    position: absolute;
+    right: -130px;
+  }
 
-}
-.price{
-  margin-left:5px;
-}
-.discount{
-  margin-left:5px;
-}
-.cuponCode{
-  margin-left:15px;
-}
-.saving{
-  color:orange
-}
-.cuponCode h1{
-  position: absolute;
-  right: -130px;
-}
-
-.remove{
-  margin-left:50px;
-  cursor: pointer;
-}
-.remove:hover{
-  color:orange
-}
-
-`
+  .remove {
+    margin-left: 50px;
+    cursor: pointer;
+  }
+  .remove:hover {
+    color: orange;
+  }
+`;
--- wooden-crafts/src/Components/CartPage/CartPage.tsx
+++ wooden-crafts/src/Components/CartPage/CartPage.tsx
@@ -1,93 +1,77 @@
 import React, { useEffect, useState } from "react";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../redux/cartReducer/cartReducer";
 import CartItem from "./CartItem";
 import { Product } from "../constrainsts/Type";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import axios from "axios";
-import { RequestAction,RequestError } from "../redux/ProductReducer/action";
+import { RequestAction, RequestError } from "../redux/ProductReducer/action";
 import { getCartProduct } from "../redux/cartReducer/action";
-
-
-
 
 // Helper function to calculate the total price of all items in the cart
 
-// function calculateTotalPrice(data:any){
+// function calculateTotalPrice(data: { product: Product; quantity: number }[]): number {
 //   let total = 0;
-//   data.reduce((item:any,ind:number) => {
-//     const price = parseInt(item.singleProductprice.replace(",", ""));
-//     total += Number(price) ;
+//   data.forEach((item) => {
+//     const price = parseInt(item.product.price.replace(",", ""));
+//     total += price * item.quantity;
 //   });
 //   return total;
 // }
 
-
-
-
 const CartPage: React.FC = () => {
-  const [data,setData]=useState<any>()
-  const [total,setTotal]=useState<any>(0)
+  const [data, setData] = useState<any>();
+  const [total, setTotal] = useState<any>(0);
   //const [qunatity,setQuantity]=useState<any>(1)
-  
 
   // const cart = useSelector((store:any) => {
   //   console.log(store)
   //   return store.cartReducer.cart
   // })
 
-  let dispatch=useDispatch()
+  let dispatch = useDispatch();
 
+  const FetchData = () => {
+    console.log("reach");
 
-  const FetchData=()=>{
+    dispatch(RequestAction());
+    axios
+      .get("https://all-products-wjqd.onrender.com/cart")
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        dispatch(getCartProduct(res.data));
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(RequestError());
+      });
+  };
 
-    console.log("reach")
-    
-    dispatch(RequestAction())
-    axios.get("https://all-products-wjqd.onrender.com/cart")
-    .then((res)=>{
-     
-      console.log(res)
-      console.log(res.data)
-      dispatch(getCartProduct(res.data))
-      setData(res.data)
-    })
-    .catch((err)=>{
-      console.log(err);
-      dispatch(RequestError())
-    })
-  
-   }
+  useEffect(() => {
+    FetchData();
+    localStorage.setItem("total", JSON.stringify(total));
+    localStorage.setItem("totalItem", JSON.stringify(data?.length));
+  }, [total]);
 
-   useEffect(()=>{
-    FetchData()
-    localStorage.setItem('total', JSON.stringify(total));
-    localStorage.setItem("totalItem",JSON.stringify(data?.length))
-
-   },[total])
-
-  
-   console.log(total)
+  console.log(total);
 
   return (
     <DIV>
-
       <div className="cart">
-
-
         <div className="cartleft1">
-
           <div className="cartLeft">
-
-            <div >
-              <h2><b>My Cart ({data?.length})</b></h2>
+            <div>
+              <h2>
+                <b>My Cart ({data?.length})</b>
+              </h2>
             </div>
 
             <div className="cl2">
               {/* <BiMap/> */}
-              <span className="deliver">Deliver to
-              </span>
+              <span className="deliver">Deliver to</span>
               <span className="check">
                 <input placeholder="Enter PinCode" />
                 <button>CHECK</button>
@@ -95,32 +79,30 @@ const CartPage: React.FC = () => {
             </div>
           </div>
 
-
-
           {/* -------------------------------------------------cart itme----------------------------------------------------- */}
 
-         {data?.map((item:any,ind:any) => (
-            <CartItem key={ind}
-             product={item.singleProduct} 
-             quantity={1} 
-            setTotal={setTotal}
-            id={item.id}
-            setData={setData}
+          {data?.map((item: any, ind: any) => (
+            <CartItem
+              key={ind}
+              product={item.singleProduct}
+              quantity={1}
+              setTotal={setTotal}
+              id={item.id}
+              setData={setData}
             />
-          ))} 
-
-          {/* <h1>total{calculateTotalPrice(data)}</h1> */}
-          
-
+          ))}
         </div>
-
 
         {/* ---------------------------------------------cart right side--------------------------------------------------- */}
         <div className="cartRight">
           <div>
             <span>Have a Coupon Code ?</span>
             <div className="cupon">
-              <input className="cuponCode" value={""} placeholder="Cupan Code" />
+              <input
+                className="cuponCode"
+                value={""}
+                placeholder="Cupan Code"
+              />
               <button>Apply</button>
             </div>
           </div>
@@ -128,145 +110,162 @@ const CartPage: React.FC = () => {
           <hr />
 
           <div className="priceDetail">
-            <h1><b>Price Detail {data?.length}</b></h1>
+            <h1>
+              <b>Price Detail {data?.length}</b>
+            </h1>
             <hr />
-            <span>MRP</span><span className="priceright">Rs 2324</span>
+            <span>MRP</span>
+            <span className="priceright">Rs 2324</span>
             <hr />
-            <span>Discount</span><span className="priceright"><span className="green">-Rs 2324</span></span>
+            <span>Discount</span>
+            <span className="priceright">
+              <span className="green">-Rs 2324</span>
+            </span>
             <hr />
-            <span><b>Total Paybale</b></span><span className="priceright">Rs {total}</span>
+            <span>
+              <b>Total Paybale</b>
+            </span>
+            <span className="priceright">Rs {total}</span>
             <hr />
-            <span className="green">Congratulations! You’ve just saved Rs 3,288 on your order.</span>
+            <span className="green">
+              Congratulations! You’ve just saved Rs 3,288 on your order.
+            </span>
           </div>
           <div>
             <button>No Cost EMI available. EMI starting Rs 1,483/month</button>
           </div>
           <div>
-            <button className="placeOrder" ><Link to="/order">Place Order</Link></button>
-
+            <button className="placeOrder">
+              <Link to="/order">Place Order</Link>
+            </button>
           </div>
         </div>
       </div>
-    </DIV >
+    </DIV>
   );
 };
 
 export default CartPage;
 
 const DIV = styled.div`
-margin-top:150px;
-  *{
-    margin-top: 0px;
-    padding:0px;
-    box-sizing:0px;
-   
+  margin-top: 130px;
+  width: 100%;
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: 0px;
   }
-  .cart{
-    margin-top:300px;
+  .cart {
+    margin-top: 300px;
     display: flex;
-    width: 80%;
+    width: 100%;
     margin: auto;
-    gap:20px;
-    background-color:white;
-  
+    background-color: white;
   }
-  .cartleft1{
-   
+  .cartleft1 {
     width: 69%;
     padding: 15px;
-    border:1px solid grey;
+    border: 1px solid grey;
     position: relative;
-    overflow-wrap:none;
+    overflow-wrap: none;
   }
 
- .cartLeft{
+  .cartLeft {
     display: flex;
+    align-items: center;
+    justify-content: space-between;
     width: 100%;
     padding: 15px;
   }
- .cl2{
-    position: absolute;
+  .cl2 {
+    /* position: absolute; */
+    display: flex;
+    align-items: center;
     right: 0px;
   }
-  .cl2 input{
+  .cl2 input {
+    height: 33px;
+    border-bottom-left-radius: 3px;
+    border-top-left-radius: 3px;
+    margin-bottom: 5px;
+    padding-left: 5px;
     width: 160px;
-    border:none;
+    border: none;
   }
-  .deliver{
-    margin-right:40px
+  .deliver {
+    margin-right: 20px;
   }
-  .check{
-    border:1px solid grey;
-    border-radius:3px;
-    padding:5px;
+  .check {
+    height: 35px;
+    display: flex;
+    align-items: center;
+    border: 1px solid grey;
+    border-radius: 3px;
+    padding: 5px;
   }
-  .check button{
-    color:#FF9800;
-   
+  .check button {
+    height: 33px;
+    color: #ff9800;
+    border-bottom-right-radius: 3px;
+    border-top-right-radius: 3px;
   }
-
-
 
   /* ------------------------------------------------------cart right------------------------------------------------------  */
-  
- .cartRight{
+
+  .cartRight {
     width: 31%;
-    border:1px solid grey;
+    border: 1px solid grey;
     height: max-content;
     text-align: left;
-    background-color:white;
-    padding:5px;
+    background-color: white;
+    padding: 5px;
   }
-  .cartRight div{
-   padding: 10px;
-   left:2px;
-   width: 100%;
-   position:relative
+  .cartRight div {
+    padding: 10px;
+    left: 2px;
+    width: 100%;
+    position: relative;
   }
-  .cupon{
-    width:max-content;
+  .cupon {
+    width: max-content;
   }
-  .cupon input{
-  width: 70%;
-  padding:15px;
-  border: 1px solid grey;
+  .cupon input {
+    width: 70%;
+    padding: 15px;
+    border: 1px solid grey;
   }
-  .cuponCode{
-  width: 70%;
-  padding: 5px;
-  border: 1px solid grey;
+  .cuponCode {
+    width: 70%;
+    padding: 5px;
+    border: 1px solid grey;
   }
-  .cupon button{
+  .cupon button {
     width: 30%;
     border: 1px solid grey;
-    margin-top:4px;
-    color:white;
-    padding:4px;
-    background-color:orange;
-   
+    margin-top: 4px;
+    color: white;
+    padding: 4px;
+    background-color: orange;
   }
 
-  .priceright{
+  .priceright {
     position: absolute;
     right: 6px;
   }
-  .green{
-    color:green;
+  .green {
+    color: green;
   }
-  hr{
-    
+  hr {
     margin: auto;
     margin: 7px;
     width: 90%;
   }
-  .placeOrder{
+  .placeOrder {
     text-align: center;
     padding: 10px;
     width: 100%;
-    background-color:orange;
-    color:white;
-    border-radius:3px;
+    background-color: orange;
+    color: white;
+    border-radius: 3px;
   }
-
-  
-`
+`;
